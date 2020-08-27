18de7f156f36776b2b84ac4bc9cd67f5ef50bde1
mayorau3 <mayo1129rau1206@gmail.com>
2020-08-27 15:25:55 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,8 @@ class MessagesController < ApplicationController
     if @message.save  # @roomに投稿された全てのmessagesを保存
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
+      # renderでindex.html.erbを参照するが、indexアクションは経由しないため、@messagesにルーム内のメッセージを代入しておく
       render :index
     end
   end
